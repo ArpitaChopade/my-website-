9810a4627d6efa8c5b477b75195184ef99f393a7
ArpitaChopade <arpita2006chopade@gmail.com>
2025-07-04 13:40:47 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+      navigator.serviceWorker.register('/service-worker.js')
+        .then(reg => console.log('Service Worker registered.', reg))
+        .catch(err => console.log('Service Worker registration failed:', err));
+    });
+  }
+  // Register service worker (you should already have this)
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+      navigator.serviceWorker.register('/service-worker.js')
+        .then(reg => {
+          console.log('Service Worker registered.', reg);
+  
+          // Now subscribe to push notifications
+          subscribeUserToPush();
+        })
+        .catch(err => console.log('Service Worker registration failed:', err));
+    });
+  }
+  
+  // Function to subscribe user to push notifications
+  function subscribeUserToPush() {
+    navigator.serviceWorker.ready.then(registration => {
+      registration.pushManager.subscribe({ userVisibleOnly: true })
+        .then(subscription => {
+          console.log('User is subscribed:', subscription);
+          // TODO: Send subscription details to your backend server here
+        })
+        .catch(err => console.error('Failed to subscribe user:', err));
+    });
+  }
+  
