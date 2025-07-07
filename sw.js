6909a19c268d4d05134acb7c80782441929b730d
ArpitaChopade <arpita2006chopade@gmail.com>
2025-07-07 12:32:08 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -12,6 +12,8 @@ self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
       return cache.addAll(urlsToCache);
+    }).catch(err => {
+      console.error('Cache addAll failed:', err);
     })
   );
 });
