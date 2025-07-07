d16ebb06d5f325c1a8c290576d44ddd7ab80a0bd
ArpitaChopade <arpita2006chopade@gmail.com>
2025-07-07 12:22:19 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,32 +1,31 @@
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
     navigator.serviceWorker.register('/my-website-/sw.js')
-      .then(reg => console.log('Service Worker registered:', reg.scope))
-      .catch(err => console.error('Service Worker failed to register:', err));
+      .then(reg => {
+        console.log('Service Worker registered with scope:', reg.scope);
+
+        // Subscribe user to push notifications
+        subscribeUserToPush();
+      })
+      .catch(err => {
+        console.error('Service Worker failed to register:', err);
+      });
   });
 }
-if ('serviceWorker' in navigator) {
-    window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/my-website-/sw.js')
-        .then(reg => {
-          console.log('Service Worker registered.', reg);
-  
-          // Now subscribe to push notifications
-          subscribeUserToPush();
-        })
-        .catch(err => console.log('Service Worker registration failed:', err));
-    });
-  }
-  
-  // Function to subscribe user to push notifications
-  function subscribeUserToPush() {
-    navigator.serviceWorker.ready.then(registration => {
-      registration.pushManager.subscribe({ userVisibleOnly: true })
-        .then(subscription => {
-          console.log('User is subscribed:', subscription);
-          // TODO: Send subscription details to your backend server here
-        })
-        .catch(err => console.error('Failed to subscribe user:', err));
-    });
+
+// Function to subscribe user to push notifications
+function subscribeUserToPush() {
+  navigator.serviceWorker.ready.then(registration => {
+    registration.pushManager.subscribe({ userVisibleOnly: true })
+      .then(subscription => {
+        console.log('User is subscribed:', subscription);
+        // TODO: Send subscription to your backend for push notifications
+      })
+      .catch(err => {
+        console.error('Failed to subscribe user:', err);
+      });
+  });
+}
+
   }
   
