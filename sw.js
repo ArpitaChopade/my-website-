fc80c259ca87e9a44724f477e6c77be1f496b553
ArpitaChopade <arpita2006chopade@gmail.com>
2025-07-07 12:01:30 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,24 +1,26 @@
-self.addEventListener('install', (event) => {
+const CACHE_NAME = 'my-pwa-cache-v1';
+const urlsToCache = [
+  '/my-website-/',
+  '/my-website-/index.html',
+  '/my-website-/manifest.json',
+  '/my-website-/styles.css',
+  '/my-website-/app.js',
+  '/my-website-/icons/icon-192.png',
+  '/my-website-/icons/icon-512.png'
+];
+
+self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open('my-pwa-cache-v1').then((cache) => {
-      return cache.addAll([
-        '/',
-        '/index.html',
-        '/manifest.json',
-        '/styles.css',
-        '/app.js',
-        '/icons/icon-192.png',
-        '/icons/icon-512.png',
-        // add any other static assets you need cached
-      ]);
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
     })
   );
 });
 
-self.addEventListener('fetch', (event) => {
+self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then((cachedResponse) => {
-      return cachedResponse || fetch(event.request);
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
     })
   );
 });
